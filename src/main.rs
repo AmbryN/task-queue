2943817ada42de1164f7da704e36f7cd8bc168ff
Nicolas AMBRY <nicolas.ambry@atos.net>
2023-08-11 11:42:46 +0200

    Got queue to work

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,110 @@
-use axum::{Router, routing::{get, post}, Json, extract::State};
-use axum_macros::debug_handler;
-use serde::{Serialize, Deserialize};
-use std::{net::SocketAddr, sync::{Arc, RwLock}};
+use serde::{Deserialize, Serialize};
+use std::{
+    sync::{mpsc::Sender, Arc, Mutex},
+    time::Duration,
+};
 
-#[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+fn main() {
+    let mut queue = Queue::new();
 
-    let shared_queue = SharedQueue::new(RwLock::new(Queue::new()));
+    let (tx_task, rx_task) = std::sync::mpsc::channel::<Arc<Mutex<Task>>>();
+    let (tx_nb, rx_nb) = std::sync::mpsc::channel::<u8>();
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/tasks", get(tasks))
-        .route("/tasks", post(create_task))
-        .with_state(Arc::clone(&shared_queue));
+    std::thread::spawn(move || loop {
+        loop {
+            let thread_task = rx_task.recv().unwrap();
+            let thread_tx_nb = tx_nb.clone();
+            std::thread::spawn(move || {
+                let mut task = thread_task.lock().unwrap();
+                let result = Some(task.compute() as usize);
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+                println!("{:?}", result);
+                task.result = result;
+                thread_tx_nb.send(1).unwrap();
+            });
+        }
+    });
 
-#[debug_handler]
-async fn tasks(state: State<SharedQueue>) -> Json<Vec<Task>> {
-    Json(state.read().unwrap().tasks.clone())
-}
+    let mut created = false;
+    loop {
+        let tx_task = tx_task.clone();
+        process(&mut queue, tx_task);
 
-#[debug_handler]
-async fn create_task(state: State<SharedQueue>, Json(payload): Json<Task>) ->  Json<Task> {
-    let task = Task {
-        duration: payload.duration
-    };
-    state.write().unwrap().enqueue(task);
-    Json(task)
+        if let Ok(val) = rx_nb.recv() {
+            queue.current_number_tasks -= val;
+        }
+
+        if !created {
+            create_task(
+                &mut queue,
+                Task {
+                    duration: 2,
+                    result: None,
+                },
+            );
+            created = true;
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 struct Task {
-    duration: usize
+    duration: usize,
+    result: Option<usize>,
+}
+
+impl Task {
+    fn compute(&self) -> u64 {
+        let duration: u64 = self.duration as u64;
+
+        std::thread::sleep(Duration::from_secs(duration));
+        return duration;
+    }
 }
 
 #[derive(Debug, Default)]
 struct Queue {
-    tasks: Vec<Task>
+    max_concurrent_tasks: u8,
+    current_number_tasks: u8,
+    index: usize,
+    tasks: Vec<Arc<Mutex<Task>>>,
 }
 
 impl Queue {
     fn new() -> Queue {
         Queue {
+            max_concurrent_tasks: 2,
+            current_number_tasks: 0,
+            index: 0,
             tasks: vec![
-                Task {
-                    duration: 3
-                },
-                Task {
-                    duration: 5
-                }
-            ]
+                Arc::new(Mutex::new(Task {
+                    duration: 3,
+                    result: None,
+                })),
+                Arc::new(Mutex::new(Task {
+                    duration: 1,
+                    result: None,
+                })),
+            ],
         }
     }
 
     fn enqueue(&mut self, task: Task) {
-        self.tasks.push(task);
+        self.tasks.push(Arc::new(Mutex::new(task)));
     }
 }
 
-type SharedQueue = Arc<RwLock<Queue>>;
+fn create_task(queue: &mut Queue, task: Task) {
+    queue.enqueue(task);
+}
 
+fn process(queue: &mut Queue, tx_task: Sender<Arc<Mutex<Task>>>) {
+    while queue.current_number_tasks < queue.max_concurrent_tasks && queue.index < queue.tasks.len()
+    {
+        let thread_task = Arc::clone(&queue.tasks[queue.index]);
+
+        tx_task.send(thread_task).unwrap();
+        queue.current_number_tasks += 1;
+        queue.index += 1;
+    }
+}
